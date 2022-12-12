27e29ad80713801b48d0ade9d4a1499aa2b4aab2
irkhamnazmi <irkham.nazmi@gmail.com>
2022-12-12 12:01:15 +0700

    Base version

---
+++ script.js
@@ -0,0 +1,102 @@
+fetch('http://localhost:5500/data.json')
+    .then(res => {
+        return res.json()
+    })
+    .then(data =>
+        get(data)
+    )
+
+
+
+
+function get(data) {
+
+    profile(data.profile)
+    event(data.event)
+    timer(data.timer)
+}
+
+function profile(data) {
+    const full = data.fullname
+    const nick = data.nickname
+    const img = data.img_profile
+    const wa = data.img_whatsapp
+    document.getElementById('title').innerHTML = `${nick} | Walimatul Khitan`
+    document.getElementById('nama').innerHTML = full
+    document.getElementById('copyright').innerHTML = `© ${full}. All rights reserved.`
+    document.getElementById('imgProfile').setAttribute('src', img)
+    document.getElementById('wa').setAttribute('data', wa)
+
+}
+
+function event(data) {
+    const bismillah = data.img_bismillah
+    const subhan = data.img_subhan
+    const day = data.day
+    const date = data.date
+    const address = data.address
+    const location = data.location
+
+    document.getElementById('bismillah').setAttribute('data', bismillah)
+    document.getElementById('subhan').setAttribute('src', subhan)
+    document.getElementById('day').innerHTML = day
+    document.getElementById('date').innerHTML = date
+    document.getElementById('address').innerHTML = address
+    document.getElementById('location').innerHTML = location
+
+
+
+}
+
+function timer(data) {
+    var countdown = function() {
+        let dayBox = document.getElementById("day-box");
+        let hrBox = document.getElementById("hr-box");
+        let minBox = document.getElementById("min-box");
+        let secBox = document.getElementById("sec-box");
+
+        // let endDate = new Date(`"19 dec 2022 8:12:00"`);
+        let endDate = new Date(`${data.day} ${data.month} ${data.year} ${data.hour}:${data.minute}:00`);
+        let endTime = endDate.getTime();
+
+        let todayDate = new Date();
+        //Output value in milliseconds
+        let todayTime = todayDate.getTime();
+        let remainingTime = endTime - todayTime;
+        //60sec => 1000 milliseconds
+        let oneMin = 60 * 1000;
+        //1hr => 60 minutes
+        let oneHr = 60 * oneMin;
+        //1 day => 24 hours
+        let oneDay = 24 * oneHr;
+
+        let addZeroes = (num) => (num < 10 ? `0${num}` : num);
+
+        if (endTime < todayTime) {
+            clearInterval(i);
+            dayBox.innerHTML = 0
+            hrBox.innerHTML = 0
+            minBox.innerHTML = 0
+            secBox.innerHTML = 0
+        } else {
+            let daysLeft = Math.floor(remainingTime / oneDay)
+            let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr)
+            let minsLeft = Math.floor((remainingTime % oneHr) / oneMin)
+            let secsLeft = Math.floor((remainingTime % oneMin) / 1000)
+
+            dayBox.innerHTML = addZeroes(daysLeft);
+            hrBox.innerHTML = addZeroes(hrsLeft);
+            minBox.innerHTML = addZeroes(minsLeft);
+            secBox.innerHTML = addZeroes(secsLeft);
+
+
+        }
+    }
+    let i = setInterval(countdown, 1000)
+    countdown()
+}
+
+function eventDetail() {
+    const element = document.getElementById("event-detail");
+    element.scrollIntoView();
+}
